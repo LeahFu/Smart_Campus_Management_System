f1a2e03e02b73f606f8b035d8ee0140cf4489814
LeahFu <121897528+LeahFu@users.noreply.github.com>
2023-05-07 12:40:43 -0400

    Create Smart_Campus_Management_System

---
+++ src/main/java/ca/access/SmartCampusManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/access/SmartCampusManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
