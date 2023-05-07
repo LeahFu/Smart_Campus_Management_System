8b14fe2cb1bdabeceee2ad6220dcc4f1983a798c
LeahFu <121897528+LeahFu@users.noreply.github.com>
2023-05-07 16:11:01 -0400

    Complete BaseEntity.java

--- src/main/java/ca/access/base/BaseEntity.java
+++ src/main/java/ca/access/base/BaseEntity.java
@@ -1,8 +1,15 @@
 package ca.access.base;
 
+import jakarta.persistence.Column;
 import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.io.File;
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.sql.Timestamp;
 
 /**
  * @author: Lei Fu
@@ -12,5 +19,48 @@ import java.io.Serializable;
 
 @Data
 public class BaseEntity implements Serializable {
+    /**
+     * create time
+     */
+    @Column(name = "create_time",nullable = false)
+    @CreationTimestamp
+    private Timestamp createTime;
 
+    /**
+     * create ID
+     */
+    @Column(name = "create_by")
+    private Long createBy;
+
+    /**
+     * update time
+     */
+    @UpdateTimestamp
+    @Column(name = "update_time")
+    private Timestamp updateTime;
+
+    @Column(name = "update_by")
+    private Long updateBy;
+
+    /**
+     * remarks
+     */
+    @Column(name = "remarks")
+    private String remarks;
+
+    public @interface Update{}
+    @Override
+    public String toString(){
+        ToStringBuilder builder = new ToStringBuilder(this);
+        Field[] fields = this.getClass().getDeclaredFields();
+        try{
+            for(Field f : fields){
+                f.setAccessible(true);
+                builder.append(f.getName(),f.get(this)).append("\n");
+            }
+        }catch (Exception e){
+            builder.append("toString builder encounter an error");
+        }
+        return builder.toString();
+    }
 }
