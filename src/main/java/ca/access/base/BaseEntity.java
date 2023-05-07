a676e2257b74785a98f5ed2904e9c08ea4b09e64
LeahFu <121897528+LeahFu@users.noreply.github.com>
2023-05-07 14:38:12 -0400

    Create BaseEntity.java

---
+++ src/main/java/ca/access/base/BaseEntity.java
@@ -0,0 +1,16 @@
+package ca.access.base;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author: Lei Fu
+ * @date: 2023/05/07
+ * @description: base(public) entity
+ */
+
+@Data
+public class BaseEntity implements Serializable {
+
+}
